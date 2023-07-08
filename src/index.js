92cf6d304e6dbed4294dbb935c60a02f585d4340
Nwosu Chukwudalu <chukwudalunwosu10@gmail.com>
2023-07-08 22:55:40 +0300

    Chat App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-const React = require('react');
-const ReactDOM = require('react-dom');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
